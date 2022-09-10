7088950ad53e36f59c418ad3e82e95c7b4b66f92
Zentor88 <43510535+Zentor88@users.noreply.github.com>
2022-09-10 03:52:16 -0500

    updating ui

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new" element={<NewMeetupsPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </>
+    </Layout>
   );
 }
 export default App;
